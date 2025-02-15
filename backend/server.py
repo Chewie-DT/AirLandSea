aa6532c07f2a196f7e75288d833a51ea44f0b266
Chewie-DT <62378845+Chewie-DT@users.noreply.github.com>
2025-02-15 21:57:56 +1000

    Update server.py
    Full deck
    Abilities

--- backend/server.py
+++ backend/server.py
@@ -14,7 +14,21 @@ FULL_DECK = [
     {"name": "Fighter Jet", "strength": 6, "theater": "Air", "ability": None},
     {"name": "Stealth Bomber", "strength": 5, "theater": "Air", "ability": "flip"},
     {"name": "Helicopter", "strength": 4, "theater": "Air", "ability": "move"},
-    # Add more cards...
+    {"name": "Air Strike", "strength": 3, "theater": "Air", "ability": "weaken"},
+    {"name": "Paratroopers", "strength": 2, "theater": "Air", "ability": "reinforce"},
+    {"name": "Spy Drone", "strength": 1, "theater": "Air", "ability": "peek"},
+    {"name": "Tank", "strength": 6, "theater": "Land", "ability": None},
+    {"name": "Artillery", "strength": 5, "theater": "Land", "ability": "weaken"},
+    {"name": "Infantry", "strength": 4, "theater": "Land", "ability": "reinforce"},
+    {"name": "Sniper", "strength": 3, "theater": "Land", "ability": "disable"},
+    {"name": "Ambush", "strength": 2, "theater": "Land", "ability": "flip"},
+    {"name": "Scout", "strength": 1, "theater": "Land", "ability": "peek"},
+    {"name": "Battleship", "strength": 6, "theater": "Sea", "ability": None},
+    {"name": "Submarine", "strength": 5, "theater": "Sea", "ability": "move"},
+    {"name": "Destroyer", "strength": 4, "theater": "Sea", "ability": "disable"},
+    {"name": "Naval Mine", "strength": 3, "theater": "Sea", "ability": "flip"},
+    {"name": "Torpedo Boat", "strength": 2, "theater": "Sea", "ability": "weaken"},
+    {"name": "Radar", "strength": 1, "theater": "Sea", "ability": "peek"},
 ]
 
 class Game:
@@ -33,6 +47,42 @@ class Game:
         random.shuffle(self.deck)
         self.hands[0] = self.deck[:6]
         self.hands[1] = self.deck[6:12]
+
+    def play_card(self, card: Dict, theater: str, player_id: int):
+        self.board[theater].append(card)
+        self.hands[player_id].remove(card)
+        self.apply_ability(card, theater, player_id)
+        self.turn = 1 - self.turn  # Switch turn
+
+    def apply_ability(self, card: Dict, theater: str, player_id: int):
+        """Apply special abilities based on card effects."""
+        opponent = 1 - player_id  # Get the opponent's ID
+
+        if card["ability"] == "flip":
+            if self.board[theater]:  
+                self.board[theater][-1]["strength"] = 0  
+
+        elif card["ability"] == "move":
+            if self.board[theater]:  
+                moved_card = self.board[theater].pop()  
+                new_theater = random.choice(["Air", "Land", "Sea"])  
+                self.board[new_theater].append(moved_card)
+
+        elif card["ability"] == "weaken":
+            if self.board[theater]:  
+                self.board[theater][-1]["strength"] = max(0, self.board[theater][-1]["strength"] - 2)
+
+        elif card["ability"] == "reinforce":
+            if self.board[theater]:  
+                self.board[theater][-1]["strength"] += 2  
+
+        elif card["ability"] == "disable":
+            self.hands[opponent] = [{"name": card["name"], "strength": card["strength"], "theater": card["theater"], "ability": None} if c == card else c for c in self.hands[opponent]]
+
+        elif card["ability"] == "peek":
+            if self.hands[opponent]:  
+                peeked_card = self.hands[opponent][0]  
+                print(f"Peeked at opponent's card: {peeked_card['name']} (Strength: {peeked_card['strength']})")
 
 @app.websocket("/game/{game_id}/{player_id}")
 async def game_socket(websocket: WebSocket, game_id: str, player_id: int):
@@ -65,9 +115,7 @@ async def game_socket(websocket: WebSocket, game_id: str, player_id: int):
                     theater = move["theater"]
 
                     if card in game.hands[player_id]:
-                        game.board[theater].append(card)
-                        game.hands[player_id].remove(card)
-                        game.turn = 1 - game.turn  # Switch turn
+                        game.play_card(card, theater, player_id)
                     else:
                         await websocket.send_json({"error": "Card not in hand"})
                         continue
